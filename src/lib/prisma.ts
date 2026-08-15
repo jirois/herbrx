2df636c1a9bbbf15dbd8d6a78143244a0db14831
Omas Ajiri <jicintech@gmail.com>
2026-08-15 23:45:49 +0100

    fix: prisma databaseUrl debuging5

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 const createPrismaClient = () => {
   const databaseUrl =
     process.env.DATABASE_URL ||
-    'mysql://u309736608_herbrx_db:U2Herbrx20260808%40127.0.0.1:3306/u309736608_herbrx'
+    'mysql://u309736608_herbrx_db:U2Herbrx20260808@127.0.0.1:3306/u309736608_herbrx'
 
   const url = new URL(databaseUrl)
 
